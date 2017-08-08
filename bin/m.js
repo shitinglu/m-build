7ff57995c8732a3244eb219dd5669046195d21e5
shitinglu <shitinglu@jd.com>
2017-08-08 17:44:07 +0800

    新增compile模块替换pack模块  pack模块独立成打包上线模块
    新增compile模块替换pack模块  pack模块独立成打包上线模块

--- bin/m.js
+++ bin/m.js
@@ -7,6 +7,7 @@ const packageJson = require('../package.json');
 const init = require('../lib/init');
 const run = require('../lib/run');
 const pack = require('../lib/pack');
+const compile = require('../lib/compile');
 
 // 此处不用简写是为了参数的统一化
 // []代表可填项   <>代表必填项
@@ -15,16 +16,27 @@ program
   .version(packageJson.version, '-v,--version')
   .option('init [value]', '初始化目录[ h5 | m ]')
   .option('run [value]', '开启服务')
-  .option('pack [value]', '打包')
+  .option('compile [value]', '编译所有文件')
+  .option('pack [value]', '打包到线上环境')
   .parse(process.argv);
 
 
 if (program.init) {
+
   init(program.init);
+
 } else if (program.run) {
+
   run(program.run);
+
 } else if (program.pack) {
+
   pack(program.pack);
-} else {
+
+} else if (program.compile) {
+
+  compile(program.compile);
+
+}else {
   console.log('无此命令');
 }
